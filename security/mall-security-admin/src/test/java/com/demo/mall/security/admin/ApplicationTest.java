a67f1a07f06429c8fad6cd34ac2497a9492556a8
cindy <cindy5832@gmail.com>
2025-04-07 18:53:42 +0800

    initial

---
+++ security/mall-security-admin/src/test/java/com/demo/mall/security/admin/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.demo.mall.security.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
